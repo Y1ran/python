2b0f1bbb5123477a53e9e6f8dc1c2c14e9690c86
hatwanghacker <deanyuton@gmail.com>
2018-01-31 20:57:38 +0800

    socket

---
+++ socket.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 20:37:36 2018
+
+@author: Administrator
+"""
+#import future
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+
+mysock.close()
