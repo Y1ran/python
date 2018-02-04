d9bf645f545123289a7f32659ee361b7636f63b7
hatwanghacker <deanyuton@gmail.com>
2018-02-04 21:52:59 +0800

    URL

---
+++ URLlib.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 04 21:44:14 2018
+
+@author: Administrator
+"""
+
+#从网页解码数据
+
+import requests, urllib
+
+fhand = urllib.requests.urlopen("http://www.baidu.com")
+
+for line in fhand.readlines():
+    print(line.decode().strip())
